7e11a16ed22c64f26ede2cad6af70fdb180e7157
Daniel Rauch <rauch.daniel1@gmx.net>
2025-04-27 18:04:01 +0200

    remove not working secret.

--- lua/chat_gpt.lua
+++ lua/chat_gpt.lua
@@ -1,7 +1,7 @@
 local chatgpt = require('chatgpt')
 
 chatgpt.setup({
-    api_key_cmd = "echo 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",
+    api_key_cmd = "",
 	 openai_params = {
 		 max_tokens = 3000,
 	 },
